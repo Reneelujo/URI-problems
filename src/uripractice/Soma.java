0ba2cd776cee2d0c4b2e09188fa125a3f3fb0cb3
Renee Lujo <reneelujo@Renees-MBP.fios-router.home>
2019-07-10 01:45:47 -0400

    first commit

---
+++ src/uripractice/Soma.java
@@ -0,0 +1,31 @@
+package uripractice;
+
+import java.util.Scanner;
+
+
+public class Soma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+
+				
+			//problem 1003
+			Scanner scan = new Scanner(System.in); 
+			
+			int A= scan.nextInt();
+			
+			int B= scan.nextInt();
+			
+			int sum= A + B;
+			
+			System.out.println("SOMA = " + sum);
+			
+		
+
+		
+		
+	}
+
+}
